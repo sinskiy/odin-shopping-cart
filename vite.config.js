dd249df95d59867dfe35329c1033be781b21697e
sinskiy <sinskiy@proton.me>
2024-08-15 09:24:46 +0300

    chore: add and configure css modules

--- vite.config.js
+++ vite.config.js
@@ -9,4 +9,9 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: "./tests/setup.js",
   },
+  css: {
+    modules: {
+      localsConvention: "camelCaseOnly",
+    },
+  },
 });
